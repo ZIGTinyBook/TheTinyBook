2456b9f81ac0886cb7a1d49169efb42c848cfe0e
Marco DV <macsdeve@gmail.com>
2024-09-09 12:24:00 +0200

    fixed issuefrom array function

--- src/Core/Tensor/tests.zig
+++ src/Core/Tensor/tests.zig
@@ -51,3 +51,37 @@ test "SetAtTest" {
     const value = try tensor.get_at(&indices);
     try std.testing.expect(value == 1.0);
 }
+
+test "test Tensor fromArray" {
+    const allocator = std.testing.allocator;
+
+    // Definisco un array multidimensionale
+    var inputArray: [2][3]f64 = [_][3]f64{
+        [_]f64{ 1.0, 2.0, 3.0 },
+        [_]f64{ 4.0, 5.0, 6.0 },
+    };
+
+    // Definisco la forma desiderata
+    var shape: [2]usize = [_]usize{ 2, 3 };
+
+    var tensor = try Tensor(f64).fromArray(&allocator, &inputArray, &shape);
+    defer tensor.deinit();
+
+    try std.testing.expectEqual(6, tensor.size);
+
+    try std.testing.expectEqual(2, tensor.shape[0]);
+    try std.testing.expectEqual(3, tensor.shape[1]);
+
+    try std.testing.expectEqual(1.0, tensor.data[0]);
+    try std.testing.expectEqual(2.0, tensor.data[1]);
+    try std.testing.expectEqual(3.0, tensor.data[2]);
+    try std.testing.expectEqual(4.0, tensor.data[3]);
+    try std.testing.expectEqual(5.0, tensor.data[4]);
+    try std.testing.expectEqual(6.0, tensor.data[5]);
+
+    const value = try tensor.get_at(&[_]usize{ 1, 2 });
+    try std.testing.expectEqual(6.0, value);
+
+    const tensorSize = tensor.getSize();
+    try std.testing.expectEqual(6, tensorSize);
+}
