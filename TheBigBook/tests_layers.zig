131fd0b99ccc4bcb892f9eb91ba4ab68df9cc564
Marco DV <macsdeve@gmail.com>
2024-09-24 16:48:06 +0200

    test passing except for layer forward because missing sum

--- TheBigBook/tests_layers.zig
+++ TheBigBook/tests_layers.zig
@@ -1,17 +1,14 @@
 const std = @import("std");
 const DenseLayer = @import("layers.zig").DenseLayer;
 const tensor = @import("tensor.zig");
-const randn = @import("layers.zig").randn;
-const zeros = @import("layers.zig").zeros;
-const TensMath = @import("tensor_math.zig");
-const Architectures = @import("architectures.zig");
 
-test "DenseLayer init and forward test" {
-    const allocator = std.heap.page_allocator;
+test "DenseLayer forward test" {
+    const allocator = &std.testing.allocator;
 
     var rng = std.rand.Random.Xoshiro256.init(12345);
 
-    var dense_layer = DenseLayer(f64, &allocator){
+    // Definizione del DenseLayer con 4 input e 2 neuroni
+    var dense_layer = DenseLayer(f64, allocator){
         .weights = undefined,
         .bias = undefined,
         .output = undefined,
@@ -19,73 +16,36 @@ test "DenseLayer init and forward test" {
         .n_neurons = 0,
         .weightShape = undefined,
         .biasShape = undefined,
-        .allocator = &allocator,
+        .allocator = allocator,
     };
 
-    try dense_layer.init(2, 2, &rng);
+    try dense_layer.init(4, 2, &rng);
 
-    std.debug.print("Dopo l'inizializzazione, pesi e bias sono stati stampati\n", .{});
+    std.debug.print("Pesi e bias inizializzati\n", .{});
 
-    var inputArray: [2][2]f64 = [_][2]f64{
-        [_]f64{ 1.0, 2.0 },
-        [_]f64{ 4.0, 5.0 },
+    // Definisci un input tensor con forma 2x4
+    var inputArray: [2][4]f64 = [_][4]f64{
+        [_]f64{ 1.0, 2.0, 3.0, 1.0 },
+        [_]f64{ 4.0, 5.0, 6.0, 2.0 },
     };
-    var shape: [2]usize = [_]usize{ 2, 2 };
+    var shape: [2]usize = [_]usize{ 2, 4 };
 
-    var t1 = try tensor.Tensor(f64).init(&allocator, shape[0..]);
-    _ = try t1.fromArray(&inputArray, shape[0..]);
+    var input_tensor = try tensor.Tensor(f64).fromArray(allocator, &inputArray, &shape);
+    defer input_tensor.deinit();
 
-    _ = try dense_layer.forward(t1); // Rimuovi il & davanti a t1
+    var output = try dense_layer.forward(&input_tensor);
+    defer output.deinit();
 
-    try std.testing.expectEqual(dense_layer.output.shape[0], 2);
-    try std.testing.expectEqual(dense_layer.output.shape[1], 2);
+    std.debug.print("Output tensor shape: {any}\n", .{output.shape});
+    std.debug.print("Output tensor data: {any}\n", .{output.data});
 
-    try std.testing.expect(dense_layer.output.data[0] != 0);
-    try std.testing.expect(dense_layer.output.data[1] != 0);
+    try std.testing.expectEqual(output.shape[0], 2);
+    try std.testing.expectEqual(output.shape[1], 2);
 
-    dense_layer.weights.deinit();
-    dense_layer.bias.deinit();
-    t1.deinit();
-}
+    try std.testing.expect(output.data[0] != 0);
+    try std.testing.expect(output.data[1] != 0);
 
-test "DenseLayer init and forward with non-square matrices and multiple dimensions" {
-    const allocator = std.heap.page_allocator;
-
-    var rng = std.rand.Random.Xoshiro256.init(12345);
-
-    var dense_layer = DenseLayer(f64, &allocator){
-        .weights = undefined,
-        .bias = undefined,
-        .output = undefined,
-        .n_inputs = 0,
-        .n_neurons = 0,
-        .weightShape = undefined,
-        .biasShape = undefined,
-        .allocator = &allocator,
-    };
-
-    try dense_layer.init(3, 2, &rng);
-
-    std.debug.print("Dopo l'inizializzazione, pesi e bias sono stati stampati\n", .{});
-
-    var inputArray: [2][3]f64 = [_][3]f64{
-        [_]f64{ 1.0, 2.0, 3.0 },
-        [_]f64{ 4.0, 5.0, 6.0 },
-    };
-    var shape: [2]usize = [_]usize{ 2, 3 };
-
-    var t1 = try tensor.Tensor(f64).init(&allocator, shape[0..]);
-    _ = try t1.fromArray(&inputArray, shape[0..]);
-
-    _ = try dense_layer.forward(t1);
-
-    try std.testing.expectEqual(dense_layer.output.shape[0], 2);
-    try std.testing.expectEqual(dense_layer.output.shape[1], 2);
-
-    try std.testing.expect(dense_layer.output.data[0] != 0);
-    try std.testing.expect(dense_layer.output.data[1] != 0);
-
-    dense_layer.weights.deinit();
-    dense_layer.bias.deinit();
-    t1.deinit();
+    dense_layer.deinit();
+    output.deinit();
+    input_tensor.deinit();
 }
