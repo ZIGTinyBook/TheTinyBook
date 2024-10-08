38ff25f1c3c57b695fb3e85ba3ecf4c0d6039a79
Marco DV <macsdeve@gmail.com>
2024-10-08 08:33:08 +0200

    dataloader fixes

--- TheBigBook/tests_model.zig
+++ TheBigBook/tests_model.zig
@@ -64,67 +64,67 @@ test "Model with multiple layers forward test" {
     model.deinit();
 }
 
-// test "Model with multiple layers training test" {
-//     std.debug.print("\n     test: Model with multiple layers training test", .{});
-//     const allocator = std.heap.page_allocator;
+test "Model with multiple layers training test" {
+    std.debug.print("\n     test: Model with multiple layers training test", .{});
+    const allocator = std.heap.page_allocator;
 
-//     var model = Model(f64, &allocator){
-//         .layers = undefined,
-//         .allocator = &allocator,
-//         .input_tensor = undefined,
-//     };
-//     try model.init();
+    var model = Model(f64, &allocator){
+        .layers = undefined,
+        .allocator = &allocator,
+        .input_tensor = undefined,
+    };
+    try model.init();
 
-//     var rng = std.Random.Xoshiro256.init(12345);
+    var rng = std.Random.Xoshiro256.init(12345);
 
-//     var layer1 = layer.DenseLayer(f64, &allocator){
-//         .weights = undefined,
-//         .bias = undefined,
-//         .output = undefined,
-//         .outputActivation = undefined,
-//         .n_inputs = 0,
-//         .n_neurons = 0,
-//         .w_gradients = undefined,
-//         .b_gradients = undefined,
-//         .allocator = undefined,
-//         .activation = undefined,
-//     };
-//     //layer 1: 3 inputs, 2 neurons
-//     try layer1.init(3, 2, &rng, "ReLU");
-//     try model.addLayer(&layer1);
+    var layer1 = layer.DenseLayer(f64, &allocator){
+        .weights = undefined,
+        .bias = undefined,
+        .output = undefined,
+        .outputActivation = undefined,
+        .n_inputs = 0,
+        .n_neurons = 0,
+        .w_gradients = undefined,
+        .b_gradients = undefined,
+        .allocator = undefined,
+        .activation = undefined,
+    };
+    //layer 1: 3 inputs, 2 neurons
+    try layer1.init(3, 2, &rng, "ReLU");
+    try model.addLayer(&layer1);
 
-//     var layer2 = layer.DenseLayer(f64, &allocator){
-//         .weights = undefined,
-//         .bias = undefined,
-//         .output = undefined,
-//         .outputActivation = undefined,
-//         .n_inputs = 0,
-//         .n_neurons = 0,
-//         .w_gradients = undefined,
-//         .b_gradients = undefined,
-//         .allocator = undefined,
-//         .activation = undefined,
-//     };
-//     //layer 2: 2 inputs, 3 neurons
-//     try layer2.init(2, 3, &rng, "ReLU");
-//     try model.addLayer(&layer2);
+    var layer2 = layer.DenseLayer(f64, &allocator){
+        .weights = undefined,
+        .bias = undefined,
+        .output = undefined,
+        .outputActivation = undefined,
+        .n_inputs = 0,
+        .n_neurons = 0,
+        .w_gradients = undefined,
+        .b_gradients = undefined,
+        .allocator = undefined,
+        .activation = undefined,
+    };
+    //layer 2: 2 inputs, 3 neurons
+    try layer2.init(2, 3, &rng, "ReLU");
+    try model.addLayer(&layer2);
 
-//     var inputArray: [2][3]f64 = [_][3]f64{
-//         [_]f64{ 1.0, 2.0, 3.0 },
-//         [_]f64{ 4.0, 5.0, 6.0 },
-//     };
-//     var shape: [2]usize = [_]usize{ 2, 3 };
+    var inputArray: [2][3]f64 = [_][3]f64{
+        [_]f64{ 1.0, 2.0, 3.0 },
+        [_]f64{ 4.0, 5.0, 6.0 },
+    };
+    var shape: [2]usize = [_]usize{ 2, 3 };
 
-//     var input_tensor = try tensor.Tensor(f64).fromArray(&allocator, &inputArray, &shape);
-//     defer input_tensor.deinit();
+    var input_tensor = try tensor.Tensor(f64).fromArray(&allocator, &inputArray, &shape);
+    defer input_tensor.deinit();
 
-//     var target_tensor = try tensor.Tensor(f64).fromArray(&allocator, &inputArray, &shape);
-//     defer target_tensor.deinit();
+    var target_tensor = try tensor.Tensor(f64).fromArray(&allocator, &inputArray, &shape);
+    defer target_tensor.deinit();
 
-//     try model.train(&input_tensor, &target_tensor, 2);
+    try model.train(&input_tensor, &target_tensor, 2);
 
-//     //std.debug.print("Output tensor shape: {any}\n", .{output.shape});
-//     //std.debug.print("Output tensor data: {any}\n", .{output.data});
+    //std.debug.print("Output tensor shape: {any}\n", .{output.shape});
+    //std.debug.print("Output tensor data: {any}\n", .{output.data});
 
-//     model.deinit();
-// }
+    model.deinit();
+}
