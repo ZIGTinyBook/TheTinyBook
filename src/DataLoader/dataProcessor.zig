0ce3f43f9126d608f6ef44cfd3f6034fcb08e273
Marco DV <macsdeve@gmail.com>
2024-10-11 09:21:19 +0200

    problems on backward of softmax

--- src/DataLoader/dataProcessor.zig
+++ src/DataLoader/dataProcessor.zig
@@ -16,35 +16,49 @@ pub fn normalize(comptime T: anytype, tensor: *Tensor(T), normalizationType: Nor
 
 // implements unity-based normalization
 fn normalizeUnityBased2D(comptime T: anytype, tensor: *Tensor(T)) !void {
-
     // --- Checks ---
-    //2D only
-    if (tensor.shape.len > 2) return error.TooManyDimensions;
-    //T must be float necessary
+    // T must be float
     if (@typeInfo(T) != .Float) return error.NotFloatType;
 
     const rows = tensor.shape[0];
-    const cols = tensor.shape[1];
+    const cols = if (tensor.shape.len == 2) tensor.shape[1] else 1;
 
-    //normalise
+    // Normalise
     var max: T = tensor.data[0];
     var min: T = tensor.data[0];
     var delta: T = 0;
 
-    for (0..rows) |i| {
-        max = tensor.data[i * cols];
-        min = tensor.data[i * cols];
-        //find max and min
-        for (0..cols) |j| {
-            if (tensor.data[i * cols + j] > max) max = tensor.data[i * cols + j];
-            if (tensor.data[i * cols + j] < min) min = tensor.data[i * cols + j];
+    // If tensor is 1D
+    if (tensor.shape.len == 1) {
+        // Find max and min for 1D tensor
+        for (0..rows) |i| {
+            if (tensor.data[i] > max) max = tensor.data[i];
+            if (tensor.data[i] < min) min = tensor.data[i];
         }
         delta = max - min;
-        std.debug.print("\n min:{} max:{} delta:{}", .{ min, max, delta });
+        std.debug.print("\n 1D min:{} max:{} delta:{}", .{ min, max, delta });
 
-        //update tensor
-        for (0..cols) |j| {
-            tensor.data[i * cols + j] = (tensor.data[i * cols + j] - min) / delta;
+        // Update tensor for 1D normalization
+        for (0..rows) |i| {
+            tensor.data[i] = (tensor.data[i] - min) / delta;
+        }
+    } else {
+        // 2D tensor case
+        for (0..rows) |i| {
+            max = tensor.data[i * cols];
+            min = tensor.data[i * cols];
+            // Find max and min for each row
+            for (0..cols) |j| {
+                if (tensor.data[i * cols + j] > max) max = tensor.data[i * cols + j];
+                if (tensor.data[i * cols + j] < min) min = tensor.data[i * cols + j];
+            }
+            delta = max - min;
+            std.debug.print("\n 2D min:{} max:{} delta:{}", .{ min, max, delta });
+
+            // Update tensor for 2D normalization
+            for (0..cols) |j| {
+                tensor.data[i * cols + j] = (tensor.data[i * cols + j] - min) / delta;
+            }
         }
     }
 }
